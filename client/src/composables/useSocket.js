9ad93cb64b906c734816b22d1318f50c6bef4a38
navinkumar.palanivel <navinkumar.palanivel@intellectdesign.com>
2025-09-27 15:48:43 +0530

    Revert to localhost server

--- client/src/composables/useSocket.js
+++ client/src/composables/useSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socket = io(import.meta.env.PROD ? 'wss://language-exchange-server.onrender.com' : 'http://localhost:8080')
+const socket = io('http://localhost:8080')
 
 export function useSocket() {
   return { socket }
